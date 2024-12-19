1c9b18ab3456f6750b3ac8d5afda8cf7a2ec9a5b
Petrando <petrandorichard@gmail.com>
2024-12-19 11:54:25 +0800

    create action.ts for signup

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       dynamic: 30,
     },
   },
-  serverExternalPackages: ["@node-rs/argon2"]
+  //serverExternalPackages: ["@node-rs/argon2"]
 };
 
 export default nextConfig;
