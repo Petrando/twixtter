ddf850719be884a9eccbf14c744727f05f3bf88b
Petrando <petrandorichard@gmail.com>
2024-12-23 13:00:23 +0800

    Navbar after login - logout function tested

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 //import MenuBar from "./MenuBar";
-//import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 import SessionProvider from "./SessionProvider";
 
 export default async function Layout({
@@ -16,7 +16,7 @@ export default async function Layout({
     return (
         <SessionProvider value={session}>
             <div className="flex min-h-screen flex-col">
-                {/*<Navbar /> */}
+                <Navbar /> 
                 <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
                 {/*<MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />*/}
                 {children}
