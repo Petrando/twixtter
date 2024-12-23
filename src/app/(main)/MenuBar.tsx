d0b8e65a2ab1a938dfb9ce03637ba1b55fb8a33f
Petrando <petrandorichard@gmail.com>
2024-12-23 16:05:28 +0800

    added MenuBar to main layout, and added posts/editor/action.ts file

---
+++ src/app/(main)/MenuBar.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { validateRequest } from "@/auth";
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+//import streamServerClient from "@/lib/stream";
+import { Bookmark, Home, Bell, Mail } from "lucide-react";
+import Link from "next/link";
+//import MessagesButton from "./MessagesButton";
+//import NotificationsButton from "./NotificationsButton";
+
+interface MenuBarProps {
+  className?: string;
+}
+
+export default async function MenuBar({ className }: MenuBarProps) {
+    /*
+    const { user } = await validateRequest();
+
+    if (!user) return null;
+
+    const [unreadNotificationsCount, unreadMessagesCount] = await Promise.all([
+        prisma.notification.count({
+            where: {
+                recipientId: user.id,
+                read: false,
+            },
+        }),
+        (await streamServerClient.getUnreadCount(user.id)).total_unread_count,
+    ]);
+    */
+    return (
+        <div className={className}>
+            <Button
+                variant="ghost"
+                className="flex items-center justify-start gap-3"
+                title="Home"
+                asChild
+            >
+                <Link href="/">
+                    <Home />
+                    <span className="hidden lg:inline">Home</span>
+                </Link>
+            </Button>
+            <Button
+                variant="ghost"
+                className="flex items-center justify-start gap-3"
+                title="Notifications"
+                asChild
+                >
+                <Link href="/notifications">
+                    <div className="relative">
+                    <Bell />                   
+                    </div>
+                    <span className="hidden lg:inline">Notifications</span>
+                </Link>
+            </Button>
+            <Button
+                variant="ghost"
+                className="flex items-center justify-start gap-3"
+                title="Messages"
+                asChild
+                >
+                <Link href="/messages">
+                    <div className="relative">
+                    <Mail />                    
+                    </div>
+                    <span className="hidden lg:inline">Messages</span>
+                </Link>
+            </Button>
+            <Button
+                variant="ghost"
+                className="flex items-center justify-start gap-3"
+                title="Bookmarks"
+                asChild
+            >
+                <Link href="/bookmarks">
+                <Bookmark />
+                <span className="hidden lg:inline">Bookmarks</span>
+                </Link>
+            </Button>
+        </div>
+    );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
+  //flex flex-col gap-8 row-start-2 items-center sm:items-start
   return (    
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Front Page
+      <main className="h-[200dvh] w-full bg-red-50">
+        <div className="w-full">
+          Front Page
+        </div>
       </main>         
   );
 }
