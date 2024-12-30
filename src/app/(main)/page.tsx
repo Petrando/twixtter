b318c8bdc5a4c10095dc187aaa9801b53a6239f6
Petrando <petrandorichard@gmail.com>
2024-12-30 12:57:17 +0800

    FollowingFeed component to see posts of people you follow

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,8 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import ForYouFeed from "./ForYouFeed";
+import FollowingFeed from "./FollowingFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   
@@ -9,7 +11,18 @@ export default function Home() {
       <main className="w-full min-w-0 flex">
         <div className="w-full min-w-0 space-y-5">
           <PostEditor />
-          <ForYouFeed />
+          <Tabs defaultValue="for-you">
+            <TabsList>
+              <TabsTrigger value="for-you">For you</TabsTrigger>
+              <TabsTrigger value="following">Following</TabsTrigger>
+            </TabsList>
+            <TabsContent value="for-you">
+              <ForYouFeed />
+            </TabsContent>
+            <TabsContent value="following">
+              <FollowingFeed />
+            </TabsContent>
+          </Tabs>
         </div>
         <TrendsSidebar />
       </main>         
