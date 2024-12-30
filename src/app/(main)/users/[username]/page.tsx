f2e6bade0ed569694f3e523ddf4dfaf34d3f7f5d
Petrando <petrandorichard@gmail.com>
2024-12-30 20:32:01 +0800

    fix: change interface syntax to match the most recent version of nextJs

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,13 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { validateRequest } from "@/auth";
-import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { getUserDataSelect } from "@/lib/types";
-import { notFound } from "next/navigation";
 import { cache } from "react";
+import { formatDate } from "date-fns";
+import type { Metadata, ResolvingMetadata } from "next";
+import { notFound } from "next/navigation";
+import { validateRequest } from "@/auth";
+import FollowButton from "@/components/FollowButton";
+import FollowerCount from "@/components/FollowerCount";
+//import Linkify from "@/components/Linkify";
+import TrendsSidebar from "@/components/TrendsSidebar";
+import UserAvatar from "@/components/UserAvatar";
+import prisma from "@/lib/prisma";
+import { FollowerInfo, getUserDataSelect, UserData } from "@/lib/types";
+import { formatNumber } from "@/lib/utils";
+//import EditProfileButton from "./EditProfileButton";
+import UserPosts from "./UserPosts";
+import { Button } from "@/components/ui/button";
 
-interface PageProps {
-    params: { username: string }; // Ensure params is a plain object with username as a string
+interface Props {
+    params: Promise<{ username: string }>;
 }
 
 const getUser = cache(async (username: string, loggedInUserId: string) => {
@@ -26,11 +36,23 @@ const getUser = cache(async (username: string, loggedInUserId: string) => {
     return user;
 });
 
-export default async function Page({ params }: PageProps) {
-    // params is directly destructured as { username }
-    const { username } = params;
+export async function generateMetadata({params}: Props): Promise<Metadata> {
 
-    // Validate request (login check)
+    const { username } = await params
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) return {};
+
+    const user = await getUser(username, loggedInUser.id);
+
+    return {
+        title: `${user.displayName} (@${user.username})`,
+    };
+}
+
+export default async function Page({ params }: Props) {
+    const { username } = await params
+
     const { user: loggedInUser } = await validateRequest();
 
     if (!loggedInUser) {
@@ -41,15 +63,79 @@ export default async function Page({ params }: PageProps) {
         );
     }
 
-    // Get the user data based on the username
     const user = await getUser(username, loggedInUser.id);
 
     return (
         <main className="flex w-full min-w-0 gap-5">
             <div className="w-full min-w-0 space-y-5">
-                {/*...some components here....*/}
+                <UserProfile user={user} loggedInUserId={loggedInUser.id} />
+                <div className="rounded-2xl bg-card p-5 shadow-sm">
+                    <h2 className="text-center text-2xl font-bold">
+                        {user.displayName}&apos;s posts
+                    </h2>
+                </div>
+                <UserPosts userId={user.id} />
             </div>
             <TrendsSidebar />
         </main>
+    );
+}
+
+interface UserProfileProps {
+    user: UserData;
+    loggedInUserId: string;
+}
+
+async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
+    const followerInfo: FollowerInfo = {
+        followers: user._count.followers,
+        isFollowedByUser: user.followers.some(
+            ({ followerId }) => followerId === loggedInUserId,
+        ),
+    };
+
+    return (
+        <div className="h-fit w-full space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+            <UserAvatar
+                avatarUrl={user.avatarUrl}
+                size={250}
+                className="mx-auto size-full max-h-60 max-w-60 rounded-full"
+            />
+            <div className="flex flex-wrap gap-3 sm:flex-nowrap">
+                <div className="me-auto space-y-3">
+                <div>
+                    <h1 className="text-3xl font-bold">{user.displayName}</h1>
+                    <div className="text-muted-foreground">@{user.username}</div>
+                </div>
+                <div>Member since {formatDate(user.createdAt, "MMM d, yyyy")}</div>
+
+                <div className="flex items-center gap-3">
+                    <span>
+                        Posts:{" "}
+                        <span className="font-semibold">
+                            {formatNumber(user._count.posts)}
+                        </span>
+                    </span>
+                    <FollowerCount userId={user.id} initialState={followerInfo} />
+                </div>
+
+                </div>
+                {user.id === loggedInUserId ? (
+                    <Button>Edit Profile</Button>
+                    ) : (
+                    <FollowButton userId={user.id} initialState={followerInfo} />
+                )}
+            </div>
+            {user.bio && (
+                <>
+                <hr />
+                {/*<Linkify>*/}
+                    <div className="overflow-hidden whitespace-pre-line break-words">
+                    {user.bio}
+                    </div>
+                {/*</Linkify>*/}
+                </>
+            )}
+        </div>
     );
 }
