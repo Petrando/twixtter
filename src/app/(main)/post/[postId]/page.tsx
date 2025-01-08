9ab619327a0a73b01f720b0cab0eb526dabde7de
Petrando <petrandorichard@gmail.com>
2025-01-08 20:12:34 +0800

    fix: post detail page interface definition

--- src/app/(main)/post/[postId]/page.tsx
+++ src/app/(main)/post/[postId]/page.tsx
@@ -13,7 +13,7 @@ import { notFound } from "next/navigation";
 import { cache, Suspense } from "react";
 
 interface PageProps {
-  params: { postId: string };
+  params: Promise<{ postId: string }>;
 }   
 
 const getPost = cache(async (postId: string, loggedInUserId: string) => {
