c4b0aff14d4bd70a5507424769cdea5c6e98ae77
Petrando <petrandorichard@gmail.com>
2025-01-08 11:11:21 +0800

    fix: when user change profile pic, delete the old profile pic in the database

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,7 +2,7 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 
 const f = createUploadthing();
 
@@ -32,6 +32,16 @@ export const fileRouter = {
         })
         .onUploadComplete(async ({ metadata, file }) => {
             // This code RUNS ON YOUR SERVER after upload
+            const oldAvatarUrl = metadata.user.avatarUrl;
+
+            if (oldAvatarUrl) {
+                const key = oldAvatarUrl.split(
+                    `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+                )[1];
+
+                await new UTApi().deleteFiles(key);
+            }
+
             const newAvatarUrl = file.url.replace(
                 "/f/",
                 `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
