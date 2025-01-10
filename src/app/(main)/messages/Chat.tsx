cf0b85f196a96b662a1381e3f921a7bfe948214f
Petrando <petrandorichard@gmail.com>
2025-01-10 10:40:26 +0800

    fix: uninstall stream-chat and stream-chat-react, then re-install stream-chat

--- src/app/(main)/messages/Chat.tsx
+++ src/app/(main)/messages/Chat.tsx
@@ -4,7 +4,7 @@
 import { Loader2 } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useState } from "react";
-import {Chat as StreamChat} from "stream-chat-react"
+//import {Chat as StreamChat} from "stream-chat-react"
 //import ChatChannel from "./ChatChannel";
 //import ChatSidebar from "./ChatSidebar";
 import useInitializeChatClient from "./useInitializeChatClient";
@@ -23,7 +23,7 @@ export default function Chat() {
     return (
         <main className="relative w-full overflow-hidden rounded-2xl bg-card shadow-sm">
         <div className="absolute bottom-0 top-0 flex w-full">
-            <StreamChat
+            {/*<StreamChat
             client={chatClient}
             theme={
                 resolvedTheme === "dark"
@@ -31,15 +31,16 @@ export default function Chat() {
                 : "str-chat__theme-light"
             }
             >
-            {/*<ChatSidebar
+            <ChatSidebar
                 open={sidebarOpen}
                 onClose={() => setSidebarOpen(false)}
             />
             <ChatChannel
                 open={!sidebarOpen}
                 openSidebar={() => setSidebarOpen(true)}
-            />*/}
+            />
             </StreamChat>
+            */}
         </div>
         </main>
     );
