d07c184d9436de146eb881d9da2cae5d72f81796
Petrando <petrandorichard@gmail.com>
2025-01-31 11:33:30 +0800

    fix: notification link into liked and commented post

--- src/app/(main)/notifications/Notification.tsx
+++ src/app/(main)/notifications/Notification.tsx
@@ -23,12 +23,12 @@ export default function Notification({ notification }: NotificationProps) {
         COMMENT: {
             message: `${notification.issuer.displayName} commented on your post`,
             icon: <MessageCircle className="size-7 fill-primary text-primary" />,
-            href: `/posts/${notification.postId}`,
+            href: `/post/${notification.postId}`,
         },
         LIKE: {
             message: `${notification.issuer.displayName} liked your post`,
             icon: <Heart className="size-7 fill-red-500 text-red-500" />,
-            href: `/posts/${notification.postId}`,
+            href: `/post/${notification.postId}`,
         },
     };
 
